a58198fa05bb56da1b21ba22bdd2aad6e2f6ccad
momen <vmomenv@gmail.com>
2024-12-02 00:04:27 +0800

    更新天气请求代码

--- src/weatherapi.cpp
+++ src/weatherapi.cpp
@@ -1,27 +1,79 @@
 #include "weatherapi.h"
 #include <QFile>
+#include <QDir>
+#include <QCoreApplication>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+#include <QDebug>
+
 WeatherAPI::WeatherAPI(QObject *parent)
-    : QObject{parent},networkManager(new QNetworkAccessManager(this)){
-    // 从配置文件读取API Key和URL
+    : QObject{parent}, networkManager(new QNetworkAccessManager(this))
+{
     apiKey = readApiKeyFromConfig();
     apiUrl = "https://api.weatherapi.com/v1/current.json";
+    connect(networkManager, &QNetworkAccessManager::finished, this, &WeatherAPI::parseWeatherResponse);
 }
 
-
-QString WeatherAPI::getCurrentWeather()
+WeatherAPI::~WeatherAPI()
 {
-    // 调用天气API获取当前天气
-    // 这里模拟返回结果
-    return "当前天气：晴，25°C";
+    delete networkManager;
 }
 
 QString WeatherAPI::readApiKeyFromConfig()
 {
-    QFile configFile("weather-config.json");
-    if(!configFile.open(QIODevice::ReadOnly)){
-        qWarning()<<"无法打开天气配置文件";
+    QFile configFile(":/config/weather-config.json");
+    if (!configFile.open(QIODevice::ReadOnly)) {
+        qWarning() << "Unable to open config file:" << configFile.errorString();
+        qWarning() << "无法打开天气配置文件";
         return QString();
     }
-    QByteArray data=configFile.readAll();
-    // qDebug()<<data;
+    QByteArray data = configFile.readAll();
+    QJsonDocument jsonDoc = QJsonDocument::fromJson(data);
+    QJsonObject jsonObj = jsonDoc.object();
+
+    return jsonObj["WEATHER_API_KEY"].toString();  // 获取API Key
 }
+
+QString WeatherAPI::buildApiUrl(const QString &location)
+{
+    return QString("%1?key=%2&q=%3").arg(apiUrl, apiKey, location);
+}
+
+void WeatherAPI::parseWeatherResponse(QNetworkReply* reply)
+{
+    if (reply->error() != QNetworkReply::NoError) {
+        qWarning() << "Network error occurred:" << reply->errorString();
+        return;
+    }
+
+    QByteArray data = reply->readAll();  // 获取响应数据
+    QJsonDocument doc = QJsonDocument::fromJson(data);
+    QJsonObject jsonObj = doc.object();
+    if (jsonObj.contains("error")) {
+        qWarning() << "Error fetching weather data:" << jsonObj["error"].toObject()["message"].toString();
+        return;
+    }
+
+    // 解析天气信息
+    QString location = jsonObj["location"].toObject()["name"].toString();
+    double tempC = jsonObj["current"].toObject()["temp_c"].toDouble();
+    QString condition = jsonObj["current"].toObject()["condition"].toObject()["text"].toString();
+
+    qDebug() << "Location:" << location;
+    qDebug() << "Temperature:" << tempC << "°C";
+    qDebug() << "Condition:" << condition;
+}
+
+QString WeatherAPI::getCurrentWeather()
+{
+
+    QString location = "auto:ip";
+    QNetworkRequest request(QUrl(buildApiUrl(location)));
+    networkManager->get(request);
+
+    return "获取天气中...";
+}
+
+
--- src/weatherapi.h
+++ src/weatherapi.h
@@ -3,14 +3,20 @@
 
 #include <QObject>
 #include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QString>
 
 class WeatherAPI : public QObject
 {
     Q_OBJECT
+
 public:
     explicit WeatherAPI(QObject *parent = nullptr);
+    ~WeatherAPI();
+
     QString getCurrentWeather();  // 获取当前天气
     QString getWeatherForecast(); // 获取未来几天的天气预报
+
 private:
     QString apiKey;   // 存储API Key
     QString apiUrl;   // 存储API URL
@@ -18,10 +24,7 @@ private:
 
     QString readApiKeyFromConfig();  // 从配置文件读取API Key
     QString buildApiUrl(const QString &location);  // 构建API请求URL
-    void parseWeatherResponse(const QByteArray &data); // 解析天气数据
-
-signals:
-
+    void parseWeatherResponse(QNetworkReply* reply); // 处理网络请求的响应
 };
 
 #endif // WEATHERAPI_H
