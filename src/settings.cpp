9f6a04d8d0505810437cedadb8ce1883b73ad8bf
momen <vmomenv@gmail.com>
2024-12-08 12:32:47 +0800

    取消对内嵌web界面

--- src/settings.cpp
+++ src/settings.cpp
@@ -1,31 +1,7 @@
 #include "settings.h"
 #include <QVBoxLayout>
-#include <QWebEngineProfile>
-#include <QWebEngineView>
+
 
 Settings::Settings(QWidget *parent) : QWidget(parent)
 {
-    // 创建QWebEngineView实例
-    webView = new QWebEngineView(this);
-    webProfile = QWebEngineProfile::defaultProfile();  // 获取默认的Web配置
-
-    // 设置页面加载和显示
-    loadSettingsPage();
-
-    // 使用垂直布局将Web视图放入窗口
-    QVBoxLayout *layout = new QVBoxLayout(this);
-    layout->addWidget(webView);
-    setLayout(layout);
-}
-
-Settings::~Settings()
-{
-    delete webView;
-}
-
-// 加载HTML设置页面
-void Settings::loadSettingsPage()
-{
-    // HTML字符串可以通过QUrl加载本地HTML文件，或者通过直接加载URL来显示远程HTML
-    webView->setUrl(QUrl("qrc:/html/settings.html"));  // 你可以根据需要调整文件路径
 }
--- src/settings.h
+++ src/settings.h
@@ -2,8 +2,6 @@
 #define SETTINGS_H
 
 #include <QWidget>
-#include <QWebEngineView>
-#include <QWebEngineProfile>
 
 class Settings : public QWidget
 {
@@ -11,13 +9,9 @@ class Settings : public QWidget
 
 public:
     explicit Settings(QWidget *parent = nullptr);
-    ~Settings();
 
 private:
-    QWebEngineView *webView;  // 用于显示HTML页面的视图
-    QWebEngineProfile *webProfile;  // 用于加载HTML的Web配置
 
-    void loadSettingsPage();  // 加载设置页面HTML
 };
 
 #endif // SETTINGS_H
