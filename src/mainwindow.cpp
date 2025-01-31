4155ee989c1e1eceb5310a590f78ae8ed8c08436
momen <vmomenv@gmail.com>
2025-01-31 16:03:50 +0800

    更新翻译prompt

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -697,8 +697,7 @@ void MainWindow::on_explainCodeButton_clicked()
 
 void MainWindow::on_translateButton_clicked()
 {
-    const QString inputEditEext = ui->inputEdit->text();
-    QString prompt = QString("请根据下面的文本进行翻译。如果文本是中文，请将其翻译为英文；如果文本是英文或其他语言，请将其翻译为中文\n%1").arg(inputEditEext);
+    ui->inputEdit->setText("I want you to act as an Chinese translator, spelling corrector and improver. I will speak to you in any language and you will detect the language, translate it and answer in the corrected and improved version of my text, in Chinese. I want you to replace my simplified A0-level words and sentences with more beautiful and elegant, upper level Chinese words and sentences. Keep the meaning same, but make them more literary. I want you to only reply the correction, the improvements and nothing else, do not write explanations. My first sentence is :");
     if(isexplainCodeMonitoring){ //如果翻译按钮开启则关闭
         on_explainCodeButton_clicked();
     }
