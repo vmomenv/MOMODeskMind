3e33e279c31eadb3ce0009c76fe4273e6a5b12de
momen <vmomenv@gmail.com>
2025-01-31 15:12:05 +0800

    设置翻译按钮和解释代码按钮相对，只允许同时开启一个

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -660,6 +660,10 @@ void MainWindow::on_explainCodeButton_clicked()
     QString prompt = QString("请阅读下面的代码并用中文进行解释。首先，识别这段代码使用的编程语言，然后提供代码的功能描述。"
                              "解释代码的结构、重要函数、变量，以及任何可能需要注意的部分。如果有必要，提供简单的示例或改进建议。\n%1").arg(inputEditEext);
     if(!isexplainCodeMonitoring){
+        if(isTranslateMonitoring){ //如果翻译按钮开启则关闭
+            on_translateButton_clicked();
+        }
+
         isexplainCodeMonitoring = true;
         ui->explainCodeButton->setStyleSheet(
             "QPushButton {"
@@ -695,6 +699,9 @@ void MainWindow::on_translateButton_clicked()
 {
     const QString inputEditEext = ui->inputEdit->text();
     QString prompt = QString("请根据下面的文本进行翻译。如果文本是中文，请将其翻译为英文；如果文本是英文或其他语言，请将其翻译为中文\n%1").arg(inputEditEext);
+    if(isexplainCodeMonitoring){ //如果翻译按钮开启则关闭
+        on_explainCodeButton_clicked();
+    }
 
     if(!isTranslateMonitoring){
         isTranslateMonitoring = true;
